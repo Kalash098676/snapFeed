45f637569a49cd8cc1a0a455c7a1f0ac82267784
Kalash098676 <kalashtyagi23339@gmail.com>
2026-07-06 23:02:33 +0530

    Add home route redirect

--- front/frontend/src/App.jsx
+++ front/frontend/src/App.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import CreatePost from './pages/CreatePost.jsx';
-import Feed from './pages/Feed.jsx';
+// import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import CreatePost from "./pages/CreatePost";
+import Feed from "./pages/Feed";
 
-const App = () => {
+function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/create-post" element={<CreatePost />} />
+        <Route path="/" element={<Navigate to="/create-post" replace />} />
         <Route path="/feed" element={<Feed />} />
+        <Route path="/create-post" element={<CreatePost />} />
       </Routes>
     </Router>
   );
-};
+}
 
 export default App;
