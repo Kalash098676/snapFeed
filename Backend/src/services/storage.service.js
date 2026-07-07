e0b9c213cde0313c6775f843e87e76780e95fef8
Kalash Tyagi <kalashtyagi23339@gmail.com>
2026-07-07 09:19:54 +0530

    Update storage.service.js

--- Backend/src/services/storage.service.js
+++ Backend/src/services/storage.service.js
@@ -1,15 +1,12 @@
 const { ImageKit } = require("@imagekit/nodejs");
 
-console.log("ImageKit Class:", ImageKit);
-
 const imagekit = new ImageKit({
   publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
   urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
 });
 
-console.log("ImageKit Instance:", imagekit);
-console.log("upload:", typeof imagekit.upload);
+console.log("Available methods:", Object.getOwnPropertyNames(Object.getPrototypeOf(imagekit)));
 
 async function uploadFile(buffer) {
   return await imagekit.upload({
