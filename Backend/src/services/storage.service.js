dd5e0ec0b2ad70b987ea0a84a38c270890e128f9
Kalash Tyagi <kalashtyagi23339@gmail.com>
2026-07-07 09:33:10 +0530

    Refactor ImageKit import and upload function

--- Backend/src/services/storage.service.js
+++ Backend/src/services/storage.service.js
@@ -1,4 +1,4 @@
-const { ImageKit } = require("@imagekit/nodejs");
+const ImageKit = require("imagekit");
 
 const imagekit = new ImageKit({
   publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
@@ -6,13 +6,13 @@ const imagekit = new ImageKit({
   urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
 });
 
-console.log("Available methods:", Object.getOwnPropertyNames(Object.getPrototypeOf(imagekit)));
-
 async function uploadFile(buffer) {
-  return await imagekit.upload({
+  const response = await imagekit.upload({
     file: buffer,
     fileName: `image-${Date.now()}.jpg`,
   });
+
+  return response;
 }
 
 module.exports = uploadFile;
